25d23e77a77f7bf6aa91eb59186d3f19dbc5c16c
Pedro Nieto Mñ <pedrete999@gmail.com>
2024-09-20 01:03:52 +0200

    Delete log.

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -15,8 +15,6 @@ async fn health_check_works() {
         .await
         .expect("Failed to execute request.");
 
-    println!("Response status: {}", response.status());
-
     // Assert
     assert!(response.status().is_success());
     assert_eq!(Some(0), response.content_length());
